81d968e98f01464f04c2d225762599c1d158f343
Matt Phillips <matt@mattphillips.io>
2017-10-17 22:49:44 +0100

    Add inquier prompt to build project config

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const inquirer = require('inquirer');
+
+const nonEmpty = input => input !== '';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Project name?',
+    validate: nonEmpty
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Project description?',
+    validate: nonEmpty
+  },
+  {
+    type: 'input',
+    name: 'author',
+    suffix: ' Format: name <email> (website)',
+    message: 'Project author?',
+    validate: nonEmpty
+  },
+  {
+    type: 'input',
+    name: 'owner',
+    message: 'Github username?',
+    validate: nonEmpty
+  }
+];
+
+inquirer.prompt(questions).then(config => {
+  console.log(config);
+});
