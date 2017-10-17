320badc8effa0bef7d71bc2832a857a658e748fd
Matt Phillips <matt@mattphillips.io>
2017-10-17 22:34:37 +0100

    Add all contributors template

---
+++ src/templates/all-contributors.js
@@ -0,0 +1,12 @@
+module.exports = ({ name, owner }) => `
+{
+  "projectName": "${name}",
+  "projectOwner": "${owner}",
+  "files": [
+    "README.md"
+  ],
+  "imageSize": 100,
+  "commit": false,
+  "contributors": []
+}
+`;
